391c78308b9f2483b8607c8968735b4376e85314
Ras96 <asymptote.k.k@gmail.com>
2022-07-26 09:58:33 +0900

    :sparkles: log ent error

--- internal/interfaces/repository/errors.go
+++ internal/interfaces/repository/errors.go
@@ -1,19 +1,28 @@
+//nolint:nlreturn
 package repository
 
 import (
+	"log"
+
 	"github.com/Ras96/go-clean-architecture-template/internal/interfaces/repository/ent"
 	"github.com/Ras96/go-clean-architecture-template/pkg/errors"
 )
 
 func convertError(err error) error {
 	switch {
+	case err == nil:
+		return nil
 	case ent.IsNotFound(err):
+		// not logging here because it's a common error
 		return errors.ErrNotFound
 	case ent.IsValidationError(err):
+		log.Printf("[ERROR]: %v\n", err)
 		return errors.ErrValidate
 	case ent.IsConstraintError(err):
+		log.Printf("[ERROR]: %v\n", err)
 		return errors.ErrConflict
 	default:
+		log.Printf("[ERROR]: %v\n", err)
 		return err
 	}
 }
